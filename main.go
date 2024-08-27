aba1e79ad7a38dec6f76699aa81f0183558a58ac
Bohdan Kyrylenko <kyrylenko.bohdan@gmail.com>
2024-08-27 10:50:18 +0100

    Add func getUsers and getUsersByID

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 )
+
+type Users struct {
+	ID           int
+	Name         string
+	Password     string
+	Email        string
+	RegisteredAT time.Time
+}
 
 func main() {
 
@@ -23,4 +33,42 @@ func main() {
 
 	fmt.Println("Connected!")
 
+	users, err := getUsers(db)
+
+	fmt.Println(users)
+
+}
+
+func getUsers(db *sql.DB) ([]Users, error) {
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	users := make([]Users, 0)
+	for rows.Next() {
+		u := Users{}
+		err := rows.Scan(&u.ID, &u.Name, &u.Password, &u.Email, &u.RegisteredAT)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+
+	}
+	return users, nil
+}
+
+func getUserByID(db *sql.DB, id int) (Users, error) {
+	var u Users
+	err := db.QueryRow("SELECT * FROM users WHERE id = $1", 1).
+		Scan(&u.ID, &u.Name, &u.Password, &u.Email, &u.RegisteredAT)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			fmt.Println("No rows found")
+		}
+		return Users{}, err
+	}
+	return u, err
 }
